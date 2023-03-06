c9d8abe07963844e75d2caaa3dd76253ef566e1b
jininono <jininono96@gmail.com>
2023-03-06 11:36:05 +0900

    fullpage 추가

--- js/store.js
+++ js/store.js
@@ -44,38 +44,6 @@ $(function(){
 	  	}, 1300);
 	});
 
-	//scroll
-	window.addEventListener("wheel", function(e){
-		e.preventDefault();
-	},{passive : false});
-
-	var $html = $("html");
- 
-	var page = 1;
-	 
-	var lastPage = $("section").length + 1;
-	 
-	$html.animate({scrollTop:0},10);
-
-	$(window).on("wheel", function(e){
- 
-		if($html.is(":animated")) return;
-	 
-		if(e.originalEvent.deltaY > 0){
-			if(page== lastPage) return;
-	 
-			page++;
-		}else if(e.originalEvent.deltaY < 0){
-			if(page == 1) return;
-	 
-			page--;
-		}
-		var posTop = (page-1) * $(window).height();
-	 
-		$html.animate({scrollTop : posTop});
-	 
-	});
-
 	//video
 	$(window).resize(function(){
 
@@ -87,5 +55,12 @@ $(function(){
 		}
 
 	}).resize();
+
+	//fullpage
+	$("#fullpage").fullpage({
+		autoScrolling: true,
+		scrollHorizontally: true,
+		responsiveWidth: 768
+	});
 	
 });
